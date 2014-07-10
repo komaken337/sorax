6ba038cc5c73b782ca6787b7e8660830c9f22e72
tize511 <tize511@gmail.com>
2014-07-10 23:04:12 +0900

    Interface, Mode, Pluginの基盤を作成

---
+++ lib/plugin.rb
@@ -0,0 +1,17 @@
+# -*- coding: shift_jis -*-
+
+module Sora
+  class Plugin
+    def initialize
+    end
+
+    def on_user_message(message)
+    end
+
+    def on_sora_message(message)
+    end
+
+    def on_event(event)
+    end
+  end
+end
---
+++ lib/sora.rb
@@ -0,0 +1,66 @@
+# -*- coding: shift_jis -*-
+
+module Sora
+  class Sora
+    def initialize(name = "Sora")
+      @interface = nil  # 実行中のInterfaceのインスタンス(Interface)
+      @modes = {}  # Modeの名前とクラスのハッシュ(Symbol => Class)
+      @plugins = {}  # 起動中のPluginの名前とインスタンスのハッシュ(Symbol => Plugin)
+      @current_mode = nil  # 現在実行中のModeのインスタンス(Mode)
+      @name = name  # Soraの表示名(String)
+
+      # 登録されたModeをハッシュに格納する
+      $SET.each_mode do |name, mode|
+        @modes[name] = mode
+      end
+
+      # 登録されたPluginのインスタンスを生成し，ハッシュに格納する
+      $SET.each_plugin do |name, plugin|
+        @plugins[name] = plugin.new
+      end
+
+      # Modeをデフォルトのものに変更
+      change_mode($SET.default_mode)
+    end
+
+    def interface=(interface)
+      @interface = interface
+    end
+
+    def plugin(name)
+      return @plugins[name]
+    end
+
+    def name
+      return @name
+    end
+
+    def change_mode(mode)
+      if @modes.include?(mode)
+        @current_mode = @modes[mode].new
+      else
+        raise "#{mode} not found."
+      end
+    end
+
+    def on_user_message(message)
+      @plugins.each do |name, plugin|
+        plugin.on_user_message(message)
+      end
+
+      on_sora_message({\
+                        from: :Sora,\
+                        string: message[:string],\
+                        time: Time.now,\
+                      })
+    end
+
+    def on_sora_message(message)
+      @interface.on_sora_message(message)
+
+      @plugins.each do |name, plugin|
+        plugin.on_sora_message(message)
+      end
+    end
+  end
+end
